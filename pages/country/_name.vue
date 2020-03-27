4ff66c733f77bdd2dfafb6b4535857f5e08e1bcd
pasDamola <oyindamola.oyetunmibi@outsourceglobal.com>
2020-03-27 03:26:19 -0700

    detail page

--- pages/country/_name.vue
+++ pages/country/_name.vue
@@ -9,8 +9,28 @@
       <v-flex sm12 md6>
         <img :src="oneCountry.flag" style="max-width: 100%" />
       </v-flex>
-      <v-flex sm12 md6>
-        <img :src="oneCountry.flag" style="max-width: 100%" />
+      <v-spacer></v-spacer>
+      <v-flex sm6 md3>
+        <p>
+          <b style="font-weight: 800, font-size: 100px">{{
+            oneCountry.name
+          }}</b>
+        </p>
+        <p><b>Native Name:</b>{{ oneCountry.nativeName }}</p>
+        <p><b>Population:</b>{{ oneCountry.population }}</p>
+        <p><b>Region:</b>{{ oneCountry.region }}</p>
+        <p><b>Sub Region:</b>{{ oneCountry.subregion }}</p>
+        <p><b>Capital:</b>{{ oneCountry.capital }}</p>
+      </v-flex>
+      <v-flex sm6 md3>
+        <p><b>Top Level Domain:</b>{{ oneCountry.topLevelDomain[0] }}</p>
+        <p><b>Currencies:</b>{{ oneCountry.currencies[0]['name'] }}</p>
+        <div>
+          <b>Languages:</b>
+          <span v-for="(language, id) in oneCountry.languages" :key="id">
+            {{ language['name'], }}
+          </span>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
